8823b97cac9acc818bade0f62a6dc75df16b6f1b
Subhodeep Moitra <smoitra@cs.cmu.edu>
2012-02-24 09:52:42 -0500

    Added NP reg

--- 10702/hw3/spam.R
+++ 10702/hw3/spam.R
@@ -1,6 +1,20 @@
+library("np")
 print(getwd())
 d <- 30
 N <- 100
 X <- matrix(runif(d*N),N)
+Y = rep(0,N)
+bw_d = rep(0,d)
+
+for (i in 1:N) {
+ Y[i] <- 3*X[i,1] + cos(5*X[i,2]) + exp(X[i,3]) + rnorm(1)
+}
+
+for (j in 1:5) {
+	x_j <- X[,j]
+	model.np <- npreg(Y ~ x_j,residuals=TRUE)
+	bw_d[j] <- model.np$bw
+	
+}
 
 
