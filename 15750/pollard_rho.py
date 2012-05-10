170bf31b13dd4ade828cc2916b63b403c3732fd8
Subhodeep Moitra <smoitra@cs.cmu.edu>
2012-05-10 18:18:17 -0400

    Added seed as input

--- 15750/pollard_rho.py
+++ 15750/pollard_rho.py
@@ -2,29 +2,31 @@ import sys
 from fractions import gcd
 
 
+PRINT_TRACE = True
+
 def pseudo(x,N)  :
 	""" Generates a pesudo random number"""
 	return (x**2+1)%N
 
 def pollard(N) :
 	""" The pollard rho algorithm """
-	left = 2
-	right = 2
+	left = right = seed
 	d = 1
 	while d == 1: 
 		left = pseudo(left,N)
 		right = pseudo(pseudo(right,N),N)
 		d = gcd(abs(left-right),N)
-	if d == N :
+		if PRINT_TRACE : 
+			print(left,right,d)
 		print("Failure")
 	else :
 		print("non trivial factor is %d"%d)
 
 
 if __name__ == '__main__' :
-	N = sys.argv[1]
+	N,seed = sys.argv[1:]
 	try :
-		N = int(N)
+		N,seed = int(N),int(seed)
 	except ValueError : 
-		print """ $ python pollard_rho <composite-integer> """
+		print """ $ python pollard_rho <composite-integer> <seed>"""
 	pollard(N)
